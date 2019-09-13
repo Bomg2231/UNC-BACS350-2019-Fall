7db91956e0c438c7a63da18cf2049f1e16488647
Bomg2231 <47306983+Bomg2231@users.noreply.github.com>
2019-09-13 14:40:44 -0600

    Update index.php

--- public_html/bacs350/Superhero/index.php
+++ public_html/bacs350/Superhero/index.php
@@ -1,7 +1,1 @@
-<?php
-    $site_title = 'UNC BACS 350';
-    $page_title = 'Superhero Gallery';
-    $content = 'No content yet';
-    include 'views.php';
-    echo render_page($site_title, $page_title, $content);
-?>
+<p>time to be a hero</p>
